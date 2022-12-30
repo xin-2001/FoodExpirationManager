ed47aed060127d6791c336932400669b712f1913
xin <77916095+xin-2001@users.noreply.github.com>
2022-12-31 03:49:56 +0800

    <feat>選單功能
    主頁選單功能撰寫
    意見反映介面撰寫

--- app/src/main/java/com/example/foodexpirationmanager/formPageActivity.java
+++ app/src/main/java/com/example/foodexpirationmanager/formPageActivity.java
@@ -1,14 +1,87 @@
 package com.example.foodexpirationmanager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class formPageActivity extends Activity {
+
+    private Button menuButton;
+    private LinearLayout menuLayout;
+    private boolean menu_Bool=false;
+    private TextView homeButton;
+    private TextView totalButton;
+    private TextView searchButton;
+    private TextView formButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_page);
+
+        menuButton=findViewById(R.id.menu_Button);
+        menuLayout=(LinearLayout)findViewById(R.id.menu_Layout);
+        homeButton=findViewById(R.id.home_Button);
+        totalButton=findViewById(R.id.total_Button);
+        searchButton=findViewById(R.id.search_Button);
+        formButton=findViewById(R.id.form_Button);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(formPageActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+        totalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(formPageActivity.this,listPageActivity.class);
+                startActivity(i);
+            }
+        });
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(formPageActivity.this,searchPageActivity.class);
+                startActivity(i);
+            }
+        });
+        formButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+        //目錄的開啟及收合
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(menu_Bool==false){
+                    menuLayout.setTranslationX(0);
+                    LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) menuLayout.getLayoutParams();
+                    params.width = 500;
+                    menuLayout.setLayoutParams(params);
+                    menu_Bool=true;
+                }else{
+                    menuLayout.setTranslationX(-1);
+                    LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) menuLayout.getLayoutParams();
+                    params.width = 1;
+                    menuLayout.setLayoutParams(params);
+                    menu_Bool=false;
+                    //還要寫一個點其他地方目錄收起來的
+                }
+
+
+            }
+        });
     }
 }
