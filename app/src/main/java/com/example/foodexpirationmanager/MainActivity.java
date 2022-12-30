6970203aaee6dbc22e0a95964b102d247f1d2dfc
xin <77916095+xin-2001@users.noreply.github.com>
2022-12-30 22:43:55 +0800

    <feat>新增介面
    設置首頁介面(list尚未完成)
    建立興增頁面

--- app/src/main/java/com/example/foodexpirationmanager/MainActivity.java
+++ app/src/main/java/com/example/foodexpirationmanager/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.foodexpirationmanager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button homeButton;
+    private Button addFoodButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeButton=findViewById(R.id.home_Botton);
+        addFoodButton=findViewById(R.id.addfood_Botton);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+        addFoodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,dataInsertPageActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
