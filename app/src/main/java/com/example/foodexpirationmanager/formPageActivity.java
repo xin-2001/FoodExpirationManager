1054913a1d9376e7ad2a8b3d55ebc5311f2feb75
xin <77916095+xin-2001@users.noreply.github.com>
2022-12-31 00:05:45 +0800

    <feat>新增頁面
    建立搜尋介面
    建立意見反映意見
    建立總覽介面(list尚未完成)

---
+++ app/src/main/java/com/example/foodexpirationmanager/formPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodexpirationmanager;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class formPageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_page);
+    }
+}
---
+++ app/src/main/java/com/example/foodexpirationmanager/listPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodexpirationmanager;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listPageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_page);
+    }
+}
